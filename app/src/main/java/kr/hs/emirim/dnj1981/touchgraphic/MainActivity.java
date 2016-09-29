27c53a030f4c7f5bbf1971f17ee271ced479dea3
SeonYeong Won <dnj1981@naver.com>
2016-09-29 19:10:58 +0900

    #2 Add String resource and change layout

--- app/src/main/java/kr/hs/emirim/dnj1981/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dnj1981/touchgraphic/MainActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.dnj1981.touchgraphic;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,10 +24,20 @@ public class MainActivity extends AppCompatActivity {
         }
         protected void onDraw(Canvas canvas){
             super.onDraw(canvas);
+
             float cx=getWidth()/2.0f;
             float cy=getHeight()/2.0f;
+
             Paint paint=new Paint();
-            canvas.drawCircle(cx, cy, 50, paint);
+            paint.setStrokeWidth(7);
+            paint.setColor(Color.BLUE);
+            paint.setStyle(Paint.Style.STROKE);
+
+            canvas.drawCircle(cx, cy, 200, paint);
+
+            paint.setColor(Color.MAGENTA);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawRect(100,100,500,250,paint);
         }
     }
 
